bc69367b6fef6b594b6a60320b62f14de59259cd
Karol Majta <karolmajta@gmail.com>
2017-10-17 18:23:46 +0000

    JS

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const I = require('immutable');
+const data = require('./data');
+
+consdata.generateAssignments().toKeyedSeq()).sortBy(p => I.List(p).get(0)).forEach((p) => {
+  let user = I.List(p).get(0);
+  let timetable = I.List(p).get(1);
+});
